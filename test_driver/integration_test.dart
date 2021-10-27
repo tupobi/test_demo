de9298c41a1dfb782a3cef21ef5fef70093c6747
nanfei <lizijie@bilibili.com>
2021-10-27 14:53:27 +0800

    integration test driver.

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,8 +1,8 @@
 // import 'package:flutter_driver/driver_extension.dart';
 import 'package:integration_test/integration_test_driver.dart';
 
-// use this cmd to integration test.
-// flutter drive --driver=integration_test/integration_driver.dart --target integration_test/app_test.dart --use-existing-app=ws://127.0.0.1:52756/7G6XpOvjkDI=/ws
+// use this cmd to integration test:
+// flutter drive --driver=test_driver/integration_test.dart --target integration_test/app_test.dart --use-existing-app=ws://127.0.0.1:61144/KkxWF_gIdAI=/ws
 Future<void> main() {
   // enableFlutterDriverExtension();
   return integrationDriver();
